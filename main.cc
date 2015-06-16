1b295ae8262c97cc5338ddea3b4acecbc5e19965
bertas55 <bertas55@gmail.com>
2015-06-17 00:45:01 +0200

    commit

--- main.cc
+++ main.cc
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void shrinkbmp24(unsigned char*, long, unsigned char*, unsigned int, unsigned int);
+extern void shrinkbmp24(unsigned char *, unsigned char*, unsigned int, unsigned int, int);
 
 int main(int argc, char * argv[])   
 {
     unsigned int scale_num, scale_den;
-    unsigned char *buffer, *newImage;
-    int i, filelen;
+    int i, size, actWidth = 0, newWidth = 0, tmpInt = 0;
+    long fileLength;
+    float scale;
+
+    FILE *outputImage, *inputImage;
+    unsigned char *inputImageArray, *outputImageArray;
     
     if (argc <= 3)
     {
@@ -15,38 +19,72 @@ int main(int argc, char * argv[])
     }
     else
     {
-        FILE *img;
-        if ((img = fopen(argv[1], "r+")) == NULL)
+        
+        if ((inputImage = fopen(argv[1], "r+")) == NULL)
         {
             printf ("Nie mogę otworzyć pliku %s \n", argv[1]);
         }
         else
         {
-            FILE *out;
-            out = fopen("out.bmp", "w");
+            outputImage = fopen("out.bmp", "w");
+            fseek(inputImage, 0, SEEK_END);          // Jump to the end of the file
+            fileLength = ftell(inputImage);             // Get the current byte offset in the file
+            rewind(inputImage);                      // Jump back to the beginning of the file
             
-            fseek(img, 0, SEEK_END);          // Jump to the end of the file
-            filelen = ftell(img);             // Get the current byte offset in the file
-            rewind(img);                      // Jump back to the beginning of the file
+            inputImageArray = (unsigned char *)malloc((fileLength+1)*sizeof(unsigned char)); // Enough memory for file + \0
+            fread(inputImageArray, fileLength, 1, inputImage);
             
-            buffer = (unsigned char *)malloc((filelen+1)*sizeof(unsigned char)); // Enough memory for file + \0
-            fread(buffer, filelen, 1, img);
-            
-            newImage = (unsigned char*)malloc(sizeof(unsigned char) * filelen);
+            //ustalic prawidlowy (nowy) rozmiar
+            outputImageArray = (unsigned char*)malloc(sizeof(unsigned char) * fileLength);
 
-            
             scale_num = atoi(argv[2]);
             scale_den = atoi(argv[3]);
+
+            scale = (float)scale_num / (float)scale_den;
+
+            i = 0;
+            outputImageArray[i] = inputImageArray[i];
+            i++;
+            outputImageArray[i] = inputImageArray[i];
+            i++;
+
+            size = size + (inputImageArray[i] / 16) * 16;
+            size = size + (inputImageArray[i] % 16);
+            outputImageArray[i] = inputImageArray[i];
+
+            i++;
             
+            size = size + (inputImageArray[i] / 16) * 16*16*16;
+            size = size + (inputImageArray[i] % 16) * 16*16;
+            outputImageArray[i] = inputImageArray[i]; 
+            i++;
+
+            size = size + (inputImageArray[i] / 16) * 16*16*16*16*16;
+            size = size + (inputImageArray[i] % 16) * 16*16*16*16;
+            outputImageArray[i] = inputImageArray[i];
+            i++;
+
+            size = size + (inputImageArray[i] / 16) * 16*16*16*16*16*16*16;
+            size = size + (inputImageArray[i] % 16) * 16*16*16*16*16*16;
+
+            while (i < 54)
+            {
+                outputImageArray[i] = inputImageArray[i];
+                i++; 
+            }
+
+
+
+            printf("i = %d\n", i);
+
+
+
             
+            shrinkbmp24(inputImageArray, outputImageArray, scale_num, scale_den, size);
             
-            shrinkbmp24(buffer, filelen, newImage, scale_num, scale_den);
-            
-            fwrite(newImage, filelen, 1, out);
-            
-            fclose (img); /* zamknij plik */
-            fclose (out); /* zamknij plik */
-            
+            fwrite(outputImageArray, fileLength, 1, outputImage);
+            fclose(outputImage);
+            fclose (inputImage); /* zamknij plik */
         }
     }
    
--- shrinkbmp24.cc
+++ shrinkbmp24.cc
@@ -1,194 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void shrinkbmp24(unsigned char *buffer, long filelen, unsigned char *newImage, unsigned int scale_num, unsigned int scale_den)
+void shrinkbmp24(unsigned char *buffer, unsigned char *newImage, unsigned int scale_num, unsigned int scale_den, int size)
 {
-	long i, j;
-        float scale = (float)scale_num / (float)scale_den;
-        int newWidth = 0, newHeight = 0, tmpInt, actWidth = 0, actHeight = 0, y, offset = 0, size = 0, actPadding, newPadding, pixel, nearestMatch, cy, cx;
-        int padding_new = 0, padding_act = 0;
+    float scale = (float)scale_num / (float)scale_den;
+    int newHeight = 0, newWidth, actWidth, tmpInt, actHeight = 0, offset = 0, actPadding, newPadding;
+    int padding_new = 0, padding_act = 0, cy, cx;
     
-        
-	i = 0;
-    newImage[i] = buffer[i];
-    i++;
-   newImage[i] = buffer[i];
-   i++;
-
-            size = size + (buffer[i] / 16) * 16;
-            size = size + (buffer[i] % 16);
-            newImage[i] = buffer[i];
-
-            i++;
-            
-            size = size + (buffer[i] / 16) * 16*16*16;
-            size = size + (buffer[i] % 16) * 16*16;
-            newImage[i] = buffer[i]; 
-            i++;
-
-            size = size + (buffer[i] / 16) * 16*16*16*16*16;
-            size = size + (buffer[i] % 16) * 16*16*16*16;
-            newImage[i] = buffer[i];
-            i++;
-
-            size = size + (buffer[i] / 16) * 16*16*16*16*16*16*16;
-            size = size + (buffer[i] % 16) * 16*16*16*16*16*16;
-     
-
-	while (i < 10)
-	{
-            newImage[i] = buffer[i];
-            i++; 
-	}
     
-            offset = offset + (buffer[i] / 16) * 16;
-            offset = offset + (buffer[i] % 16);
-    newImage[i] = buffer[i];
-            i++;
+    //TRZEBA DODAC POBIERANIE OFFSETU
+    //mov eax, DWORD[esi+10]
     
-            offset = offset + (buffer[i] / 16) * 16*16*16;
-            offset = offset + (buffer[i] % 16) * 16*16;
-    newImage[i] = buffer[i];
-            i++;
     
-            offset = offset + (buffer[i] / 16) * 16*16*16*16*16;
-            offset = offset + (buffer[i] % 16) * 16*16*16*16;
-    newImage[i] = buffer[i];
-            i++;
+    //i = 22 18 - 12, 22 - 25;
     
-            offset = offset + (buffer[i] / 16) * 16*16*16*16*16*16*16;
-            offset = offset + (buffer[i] % 16) * 16*16*16*16*16*16;
+    offset = 54;
+    actWidth = 1366;
+    newWidth = actWidth * scale;
+    actHeight = 768;
+    newHeight = actHeight * scale;
+    actPadding = 4 - ((actWidth * 3) % 4);
 
+    if(actPadding == 4)
+        actPadding = 0;
 
-            while (i < 18)
-                {
-                    newImage[i] = buffer[i];
-                    i++;
-                }
-
-            actWidth = actWidth + (buffer[i] / 16) * 16;
-            actWidth = actWidth + (buffer[i] % 16);
-            i++;
-
-            actWidth = actWidth + (buffer[i] / 16) * 16*16*16;
-            actWidth = actWidth + (buffer[i] % 16) * 16*16;
-            i++;
-
-            actWidth = actWidth + (buffer[i] / 16) * 16*16*16*16*16;
-            actWidth = actWidth + (buffer[i] % 16) * 16*16*16*16;
-            i++;
-
-            actWidth = actWidth + (buffer[i] / 16) * 16*16*16*16*16*16*16;
-            actWidth = actWidth + (buffer[i] % 16) * 16*16*16*16*16*16;
-            
-            newWidth = actWidth * scale;
-            tmpInt = newWidth;
-    
-            i -= 3;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-
-    
-            actHeight = actHeight + (buffer[i] / 16) * 16;
-            actHeight = actHeight + (buffer[i] % 16);
-            i++;
-
-            actHeight = actHeight + (buffer[i] / 16) * 16*16*16;
-            actHeight = actHeight + (buffer[i] % 16) * 16*16;
-            i++;
-
-            actHeight = actHeight + (buffer[i] / 16) * 16*16*16*16*16;
-            actHeight = actHeight + (buffer[i] % 16) * 16*16*16*16;
-            i++;
-
-            actHeight = actHeight + (buffer[i] / 16) * 16*16*16*16*16*16*16;
-            actHeight = actHeight + (buffer[i] % 16) * 16*16*16*16*16*16;
-            
-            newHeight = actHeight * scale;
-            tmpInt = newHeight;
-    
-            i -= 3;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-    
-            newImage[i] = (tmpInt % 16) ;
-            tmpInt /= 16;
-            newImage[i] += (tmpInt % 16)* 16;
-            tmpInt /= 16;
-            i++;
-    
-            size -= offset;
-            size /= actHeight;
-            actPadding = size % (actWidth * 3);      
-
-            newPadding = 4 - ((newWidth * 3) % 4);
-
-        while (i < offset)
-	{
-                newImage[i] = buffer[i];
-
-		i++;
-        }
-        
-        cy = 0;
+    cy = 0;
+    while(cy < newHeight)
+    {
         cx = 0;
-    
-        while(cy < newHeight)
-        {            
-            cx = 0;
-            while(cx < newWidth)
-            {
-                pixel = (cy * (newWidth *3 + newPadding)) + (cx*3);
-                nearestMatch =  (((int)(cy / scale) * (actWidth *3 + actPadding)) + ((int)(cx / scale) *3) );
-                
-                newImage[offset + pixel   ] =  buffer[offset + nearestMatch ];
-                newImage[offset + pixel + 1] =  buffer[offset + nearestMatch + 1];
-                newImage[offset + pixel + 2 ] =  buffer[offset + nearestMatch + 2];
-                
-                cx++;
-            }
+        while(cx < newWidth)
+        {
+            int pixel = (cy * (newWidth *3 + newPadding)) + (cx*3);
+            int nearestMatch =  (((int)(cy / scale) * (actWidth *3 + actPadding)) + ((int)(cx / scale) *3) );
+               
+            newImage[offset + pixel   ] =  buffer[offset + nearestMatch ];
+            newImage[offset + pixel + 1] =  buffer[offset + nearestMatch + 1];
+            newImage[offset + pixel + 2 ] =  buffer[offset + nearestMatch + 2];
             
-            padding_new += newPadding;
-            padding_act += actPadding / scale;
-            cy++;
+            cx++;
         }
-
+        
+        padding_new += newPadding;
+        padding_act += actPadding / scale;
+        cy++;
+    }
 }
