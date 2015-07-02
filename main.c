607fca6e01f864884092fc59743a526f295b6e6f
bertas55 <bertas55@gmail.com>
2015-07-02 18:23:06 +0200

    commit

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void shrinkbmp24(unsigned char *, unsigned int, unsigned int);
+void shrinkbmp24(unsigned char *, unsigned int, unsigned int);
 
-int main(int argc, char * argv[])   
+int main(int argc, char * argv[])
 {
     unsigned int scale_num, scale_den;
     int i, size = 0;
@@ -12,14 +12,14 @@ int main(int argc, char * argv[])
 
     FILE *outputImage, *inputImage;
     unsigned char *inputImageArray, *outputImageArray;
-    
+
     if (argc <= 3)
     {
         printf ("Za malo argumentow\n");
     }
     else
     {
-        
+
         if ((inputImage = fopen(argv[1], "r+")) == NULL)
         {
             printf ("Nie mogę otworzyć pliku %s \n", argv[1]);
@@ -27,10 +27,10 @@ int main(int argc, char * argv[])
         else
         {
             outputImage = fopen("out.bmp", "w");
-            fseek(inputImage, 0, SEEK_END);          
-            fileLength = ftell(inputImage);             
-            rewind(inputImage);                      
-            
+            fseek(inputImage, 0, SEEK_END);
+            fileLength = ftell(inputImage);
+            rewind(inputImage);
+
             inputImageArray = (unsigned char *)malloc((fileLength+1)*sizeof(unsigned char));
             fread(inputImageArray, fileLength, 1, inputImage);
 
@@ -40,24 +40,24 @@ int main(int argc, char * argv[])
             scale = (float)scale_num / (float)scale_den;
 
             shrinkbmp24(inputImageArray, scale_num, scale_den);
-            
+
             size += (inputImageArray[2] >> 4) << 4;
             size += (inputImageArray[2] % 16);
-             
+
             size += (inputImageArray[3] >> 4) << 12;
             size += (inputImageArray[3] % 16) << 8;
- 
+
             size += (inputImageArray[4] >> 4) << 20;
             size += (inputImageArray[4] % 16) << 16;
- 
+
             size += (inputImageArray[5] >> 4) << 28;
             size += (inputImageArray[5] % 16) << 24;
-            
+
             fwrite(inputImageArray, size, 1, outputImage);
             fclose(outputImage);
             fclose (inputImage);
         }
     }
-   
+
 	return 0;
 }
--- shrinkbmp24.c
+++ shrinkbmp24.c
@@ -4,17 +4,10 @@
 void shrinkbmp24(unsigned char *buffer, unsigned int scale_num, unsigned int scale_den)
 {
     float scale = (float)scale_num / (float)scale_den;
-    int newHeight = 0, newWidth, actWidth, tmpInt, actHeight = 0, offset = 0, actPadding, newPadding;
-    int padding_new = 0, padding_act = 0, cy, cx, cyNewWidthNewPadding, cyScaleActWidth3Scale;
-    int dupa = 0, jedenprzezscale;
-    
-    
-    //TRZEBA DODAC POBIERANIE OFFSETU
-    //mov eax, DWORD[esi+10]
-    
-    
-    //i = 22 18 - 12, 22 - 25;
-    
+    int newHeight, newWidth, actWidth, actHeight, offset, actPadding, newPadding, loopWidth, loopHeight;
+    int pixelIn, pixelOut;
+    float odwScale;
+
     offset = 54;
     actWidth = 1280;
     newWidth = actWidth * scale;
@@ -22,47 +15,38 @@ void shrinkbmp24(unsigned char *buffer, unsigned int scale_num, unsigned int sca
     newHeight = actHeight * scale;
     actPadding = 4 - ((actWidth * 3) % 4);
     newPadding = 4 - ((newWidth * 3) % 4);
-    
+
+    printf("newWidth = %d\n", newWidth);
+    printf("newHeight = %d\n", newHeight);
 
     if(actPadding == 4)
         actPadding = 0;
-    
+
     if(newPadding == 4)
         newPadding = 0;
-    //dupa
-    dupa = 0;
-    jedenprzezscale = 1 / scale;
-    dupa = 0;
-    //dupa
-    cy = 0;
+
+    odwScale = 1 / scale;
+
+    loopHeight = 0;
+
     do
     {
-        cx = 0;
-        
-        cyNewWidthNewPadding = cy * (newWidth *3 + newPadding);
-        cyScaleActWidth3Scale = (int)(cy / scale) * (actWidth *3 + actPadding);
-        
+        pixelOut = loopHeight * (newWidth *3 + newPadding);
+        pixelIn = loopHeight * odwScale * (actWidth * 3 + actPadding);
+        loopWidth = newWidth - 1;
+
         do
         {
-            int pixel = (cyNewWidthNewPadding + (cx*3));
-            int nearestMatch =  (cyScaleActWidth3Scale + ((int)(cx / scale) *3) );
-               
-            buffer[offset + pixel   ] =  buffer[offset + nearestMatch ];
-            buffer[offset + pixel + 1] =  buffer[offset + nearestMatch + 1];
-            buffer[offset + pixel + 2 ] =  buffer[offset + nearestMatch + 2];
-            
-            //cx++;
-            //dupa
-            dupa = 0;
-            cx += jedenprzezscale;
-            dupa = 0;
-            //dupa
-            dupa++;
-        } while(cx < newWidth);
-        
-        cy++;
-    } while(cy < newHeight);
-    
-    printf("%d\n", dupa);
-    
+            buffer[offset + pixelOut    ] =  buffer[offset + pixelIn    ];
+            buffer[offset + pixelOut + 1] =  buffer[offset + pixelIn + 1];
+            buffer[offset + pixelOut + 2] =  buffer[offset + pixelIn + 2];
+
+            loopWidth--;
+            pixelIn += odwScale * 3;
+            pixelOut += 3;
+        } while(loopWidth >= 0);
+
+        loopHeight++;
+    } while(loopHeight < newHeight);
+
 }
